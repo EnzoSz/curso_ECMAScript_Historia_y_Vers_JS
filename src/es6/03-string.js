1de68cbb9acb5fc3e56c3144d139c38d69563478
Enzo Soliz <enzosoliz95@gmail.com>
2023-01-18 17:37:30 -0300

    es6, Strings

---
+++ src/es6/03-string.js
@@ -0,0 +1,18 @@
+let hello = 'Hello';
+let wolrd = 'wolrd';
+let epicPhrase = hello + '' + wolrd + '!';
+console.log(epicPhrase);
+
+//template leterals
+
+let epicPhrase2= `${hello} ${wolrd}!`;
+console.log(epicPhrase2);
+
+//multi-line strings
+let lorem = 'esto es un string \n' + 'esto es otra linea';
+
+let lorem2 = `esta es una frase epica
+la continuacion de esa frase epica.
+`;
+console.log(lorem);
+console.log(lorem2);
