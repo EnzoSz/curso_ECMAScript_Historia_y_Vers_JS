ca2ed4ab733eeac1def6858386c9cb52b8b550c4
Enzo Soliz <enzosoliz95@gmail.com>
2023-01-19 22:57:19 -0300

    es12, numeric-separator y replaceAll

---
+++ src/es12/01-replaceAll.js
@@ -0,0 +1,5 @@
+const string = "JavaScript es un maravillo lenguaje de programacion";
+
+const repleceString = string.replace("JavaScript","TypeScript");
+console.log(string);
+console.log(repleceString);
